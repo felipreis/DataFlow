05707bc864dd15e8fb2bc44ba032bba5c3ad9c7e
felipreis <felipreisdaniel@gmail.com>
2026-07-14 20:56:51 -0300

    conexão com banco de dados estabelecida

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config'
 import router from './routes/routes.js';
+import sequelize from './config/dataBaseConfig.js'
 
 
 const app = express();
@@ -10,4 +11,15 @@ app.use(express.json());
 app.use(router);
 
 
-app.listen(PORT, () => {console.log(`Servidor online at PORT ${PORT}`)})
+async function startServer(){
+    try {
+        app.listen(PORT, () => {console.log(`Servidor online at PORT ${PORT}`)})
+
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+startServer();
---
+++ src/config/testConnection.js
@@ -0,0 +1,13 @@
+import sequelize from './dataBaseConfig.js'
+
+async function testConnection(){
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+testConnection();
+
