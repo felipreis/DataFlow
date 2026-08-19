513c46fecf9e421c1e6f06695bff0f83f38c3cbf
felipreis <felipreisdaniel@gmail.com>
2026-08-18 23:58:54 -0300

    mock lead service para teste post webhook meta

--- src/services/webhook/MetaWebhookService.js
+++ src/services/webhook/MetaWebhookService.js
@@ -6,11 +6,26 @@ async function receive(payload,api_key){
     const organization = await OrganizationService.findByApiKey(api_key);
     if(!organization){throw new Error('Organização não encontrada')}
 
+
+        console.log(
+        '[META WEBHOOK]',
+        JSON.stringify(payload, null, 2)
+    );
+
+    return {
+        received: true,
+        organization_id: organization.id,
+        payload
+    };
+    
+    //mock da chjamada do service
+    /*
     const mappedLead = map(payload);
     mappedLead.organization_id = organization.id;
 
 
     return await LeadService.create(mappedLead)
+    */
 
 }
 
